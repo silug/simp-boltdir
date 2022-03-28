79e48d83c75185a396ec72cb70c36ce4fe935f82
Steven Pritchard <steve@sicura.us>
2022-03-28 12:17:43 -0500

    Default git_transport to https instead of git

--- modules/simp_update/plans/init.pp
+++ modules/simp_update/plans/init.pp
@@ -3,7 +3,7 @@
 # Wraps the simp_update task to pass the appropriate parameters.
 plan simp_update (
   TargetSpec                  $targets       = get_targets('all'),
-  Enum['git', 'ssh', 'https'] $git_transport = 'git',
+  Enum['git', 'ssh', 'https'] $git_transport = 'https',
 ) {
   run_task_with('simp_update', $targets) |$target| {
     $vars = $target.vars
